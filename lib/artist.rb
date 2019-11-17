c92afbbd34aa9ba758ccba7aed314964ce197bd8
Michael Waldo <mbwcogic@yahoo.com>
2019-11-17 17:39:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,5 +11,11 @@ class Artist
     @@all
   end
  
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
 
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
 end
